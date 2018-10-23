02ac58aac801d2558699d5fefb11e6fb6df581c0
SebastianMV <sebamunoz09@gmail.com>
2018-10-23 17:36:50 -0300

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ date_default_timezone_set('America/Santiago');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/UpNova/';
+$config['base_url'] = 'http://142.93.2.124/sebastian_munoz/UpNova/';
 
 /*
 |--------------------------------------------------------------------------
